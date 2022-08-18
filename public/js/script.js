c04d78275c1bf4cf9d076a64c4c1d231e2f9bdfc
epixieme <kirstie_hayes@hotmail.com>
2022-08-18 12:03:12 +0100

    added routes

--- public/js/script.js
+++ public/js/script.js
@@ -16,137 +16,138 @@ if (addEmotionsBtn) {
 
 if (addActionsBtn) {
   addActionsBtn.addEventListener("click", function () {
-    const newActions = actionSection.cloneNode(true); // clone the first section above with the input field inside // takes off the event handlers
-    const input = newActions.getElementsByTagName("input")[0]; // take the first input out of the section 'newEmotions' and clear it out
+    const newActions = actionSection.cloneNode(true); //on each click it creates a copy of the input field each time by cloning the first section above with the input field inside // takes off the event handlers
+    const input = newActions.getElementsByTagName("input")[0]; // take the first input field in the list out of the section 'newEmotions' and clear it out, we couldn't do this if we didn't grab just one, could have added a class or id to the input and done it that way
+    console.log(input.value)
     input.value = ""; //clear out the value just in case there is already a value
-    actionList.appendChild(newActions); //  append new feeling section and input box on click
+    actionList.appendChild(newActions); //  append new feeling section and input box on click - append the cloned field
   });
 }
 
 /////// change all of this code below/////
-const deleteEmotion = document.querySelectorAll(".deleteEmotionsBtn");
-const editEmotion = document.querySelectorAll(".editEmotionsBtn");
-const updateEmotion = document.querySelectorAll(".updateEmotionsBtn");
+// const deleteEmotion = document.querySelectorAll(".deleteEmotionsBtn");
+// const editEmotion = document.querySelectorAll(".editEmotionsBtn");
+// const updateEmotion = document.querySelectorAll(".updateEmotionsBtn");
 
-if (deleteEmotion) {
-  const deleteEmot = Array.from(deleteEmotion).forEach((element) => {
-    element.addEventListener("click", deleteEmotionsInfo); /// hears the click editGameInfo
-  });
-}
+// if (deleteEmotion) {
+//   const deleteEmot = Array.from(deleteEmotion).forEach((element) => {
+//     element.addEventListener("click", deleteEmotionsInfo); /// hears the click editGameInfo
+//   });
+// }
 
-if (editEmotion) {
-  const editEmot = Array.from(editEmotion).forEach((element) => {
-    element.addEventListener("click", editEmotionsInfo);
-  });
-}
+// if (editEmotion) {
+//   const editEmot = Array.from(editEmotion).forEach((element) => {
+//     element.addEventListener("click", editEmotionsInfo);
+//   });
+// }
 
-if (updateEmotion) {
-  const updateEmot = Array.from(updateEmotion).forEach((element) => {
-    element.addEventListener("click", updateEmotionsInfo);
-  });
-}
+// if (updateEmotion) {
+//   const updateEmot = Array.from(updateEmotion).forEach((element) => {
+//     element.addEventListener("click", updateEmotionsInfo);
+//   });
+// }
 
-function emotionsVariables() {
-  const fields = document.querySelectorAll(".userInputs"); // find a way to seperate the emotions from the actions
+// function emotionsVariables() {
+//   const fields = document.querySelectorAll(".userInputs"); // find a way to seperate the emotions from the actions
 
-  return fields;
-}
+//   return fields;
+// }
 
-function actionVariables() {
-  const actionFields = document.querySelectorAll(".actionInputs");
-  return actionFields;
-}
+// function actionVariables() {
+//   const actionFields = document.querySelectorAll(".actionInputs");
+//   return actionFields;
+// }
 
-function editEmotionsInfo() {
-  const fields = emotionsVariables();
-  const actionFields = actionVariables();
-  console.log(fields);
-  const target = this;
-  if (target.classList.contains("editEmotionsBtn")) {
-    fields.forEach((item) => {
-      // grab all of the text fields and set them to editbale etc for the name, desc and feelings
-      item.setAttribute("contenteditable", true);
-      item.style.background = "#C8CFD2";
-      // item.style.border = "1px solid grey";
-      item.style.borderRadius = "10px";
-    });
-    actionFields.forEach((item) => {
-      // grab all of the text fields and set them to editbale etc for the actions
-      item.setAttribute("contenteditable", true);
-      // item.style.border = "1px solid grey";
-       item.style.background = "#C8CFD2";
-      item.style.borderRadius = "10px";
-    });
-  }
-}
+// function editEmotionsInfo() {
+//   const fields = emotionsVariables();
+//   const actionFields = actionVariables();
+//   console.log(fields);
+//   const target = this;
+//   if (target.classList.contains("editEmotionsBtn")) {
+//     fields.forEach((item) => {
+//       // grab all of the text fields and set them to editbale etc for the name, desc and feelings
+//       item.setAttribute("contenteditable", true);
+//       item.style.background = "#C8CFD2";
+//       // item.style.border = "1px solid grey";
+//       item.style.borderRadius = "10px";
+//     });
+//     actionFields.forEach((item) => {
+//       // grab all of the text fields and set them to editbale etc for the actions
+//       item.setAttribute("contenteditable", true);
+//       // item.style.border = "1px solid grey";
+//        item.style.background = "#C8CFD2";
+//       item.style.borderRadius = "10px";
+//     });
+//   }
+// }
 
-async function updateEmotionsInfo() {
-  const fields = emotionsVariables();
-  const actionFields = actionVariables();
-  const target = this;
-  const id = fields[0].innerText;
-  console.log(id)
-  const name = fields[1].innerText;
-  const description = fields[2].innerText;
-  const feelings = Array.from(fields)
-    .map((item, index) => (index > 2 ? item.innerText : ""))
-    .filter(String);
+// async function updateEmotionsInfo() {
+//   const fields = emotionsVariables();
+//   const actionFields = actionVariables();
+//   const target = this;
+//   const id = fields[0].innerText;
+//   console.log(id)
+//   const name = fields[1].innerText;
+//   const description = fields[2].innerText;
+//   const feelings = Array.from(fields)
+//     .map((item, index) => (index > 2 ? item.innerText : ""))
+//     .filter(String);
 
-  const actions = Array.from(actionFields).map((item) => item.innerText);
-  console.log(actions);
+//   const actions = Array.from(actionFields).map((item) => item.innerText);
+//   console.log(actions);
 
-  if (target.classList.contains("updateEmotionsBtn")) {
-    fields.forEach((item) => {
-      item.setAttribute("contenteditable", false);
-      item.style.background= "none";
-      item.style.borderRadius = "10px";
-    });
+//   if (target.classList.contains("updateEmotionsBtn")) {
+//     fields.forEach((item) => {
+//       item.setAttribute("contenteditable", false);
+//       item.style.background= "none";
+//       item.style.borderRadius = "10px";
+//     });
 
-    actionFields.forEach((item) => {
-      item.setAttribute("contenteditable", false);
-      item.style.background= "none";
-      item.style.borderRadius = "10px";
-    });
-  }
+//     actionFields.forEach((item) => {
+//       item.setAttribute("contenteditable", false);
+//       item.style.background= "none";
+//       item.style.borderRadius = "10px";
+//     });
+//   }
 
-  try {
-    await fetch("/emotion/:id", {
-      method: "put",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        id: id,
-        // Send these as a string to the server
-        name: name, // have to find a way to update the field on input
-        description: description,
-        feelings: feelings,
-        actions: actions,
-      }),
-    });
-  } catch (err) {
-    console.log(err);
-  }
-}
-async function deleteEmotionsInfo() {
-  const fields = emotionsVariables();
+//   try {
+//     await fetch("/emotion/:id", {
+//       method: "put",
+//       headers: { "Content-Type": "application/json" },
+//       body: JSON.stringify({
+//         id: id,
+//         // Send these as a string to the server
+//         name: name, // have to find a way to update the field on input
+//         description: description,
+//         feelings: feelings,
+//         actions: actions,
+//       }),
+//     });
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
+// async function deleteEmotionsInfo() {
+//   const fields = emotionsVariables();
 
-  // console.log(document.querySelectorAll(".elements")); /// how can I make this section dryer - maybe an object? could I use this query selector and a conidtion? https://bobbyhadz.com/blog/javascript-get-data-attribute-from-event-object
-  // grabs all of the dom element (span) innertext for the target
+//   // console.log(document.querySelectorAll(".elements")); /// how can I make this section dryer - maybe an object? could I use this query selector and a conidtion? https://bobbyhadz.com/blog/javascript-get-data-attribute-from-event-object
+//   // grabs all of the dom element (span) innertext for the target
 
-  //   const name = target.parentNode.parentNode.childNodes[5].innerText
-  const id = fields[0].innerText;
-  console.log(name);
-  try {
-    const response = await fetch("/delete-emotion", {
-      method: "delete",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        // Send these as a string to the server
-        id: id,
-      }),
-    });
-    const data = await response.json();
-    location.reload();
-  } catch (err) {}
-}
+//   //   const name = target.parentNode.parentNode.childNodes[5].innerText
+//   const id = fields[0].innerText;
+//   console.log(name);
+//   try {
+//     const response = await fetch("/delete-emotion", {
+//       method: "delete",
+//       headers: { "Content-Type": "application/json" },
+//       body: JSON.stringify({
+//         // Send these as a string to the server
+//         id: id,
+//       }),
+//     });
+//     const data = await response.json();
+//     location.reload();
+//   } catch (err) {}
+// }
 
 
